b70dcbdef057624d11d2de158c540230e9dbfc72
945U945 <121045829+945U945@users.noreply.github.com>
2026-09-25 17:15:19 +0800

    更新: settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,29 +8,17 @@ pluginManagement {
             }
         }
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
         gradlePluginPortal()
-        mavenLocal()
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
-includeBuild("build-logic")
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        maven { url = uri("https://jitpack.io") }
         mavenCentral()
     }
 }
 
 rootProject.name = "FeishuCheckIn"
 include(":app")
-include(":hidden-api")
-include(":annotation-api")
-include(":ksp-processor")
- 
